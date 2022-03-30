be35d6af60c5c7faceae69a1dfcea3650b85e33a
matejcik <ja@matejcik.cz>
2022-03-30 16:14:38 +0200

    use modified bech32 impl with ValueErrors and bech32m support

--- src/microbitcoin/bech32.py
+++ src/microbitcoin/bech32.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Pieter Wuille
+# Copyright (c) 2017, 2020 Pieter Wuille
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -17,69 +17,104 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+from __future__ import annotations
 
-"""Reference implementation for Bech32 and segwit addresses."""
+"""Reference implementation for Bech32/Bech32m and segwit addresses.
+
+Modified to support Bech32M and saner typing properties.
+"""
+
+from enum import IntEnum
+import typing as t
+
+
+class Encoding(IntEnum):
+    """Enumeration type to list the various supported encodings."""
+
+    BECH32 = 1
+    BECH32M = 2
 
 
 CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+BECH32M_CONST = 0x2BC830A3
 
 
-def bech32_polymod(values):
+def bech32_polymod(values: list[int]) -> int:
     """Internal function that computes the Bech32 checksum."""
-    generator = [0x3B6A57B2, 0x26508E6D, 0x1EA119FA, 0x3D4233DD, 0x2A1462B3]
+    generator = [0x3B6A_57B2, 0x2650_8E6D, 0x1EA1_19FA, 0x3D42_33DD, 0x2A14_62B3]
     chk = 1
     for value in values:
         top = chk >> 25
-        chk = (chk & 0x1FFFFFF) << 5 ^ value
+        chk = (chk & 0x1FF_FFFF) << 5 ^ value
         for i in range(5):
             chk ^= generator[i] if ((top >> i) & 1) else 0
     return chk
 
 
-def bech32_hrp_expand(hrp):
+def bech32_hrp_expand(hrp: str) -> list[int]:
     """Expand the HRP into values for checksum computation."""
     return [ord(x) >> 5 for x in hrp] + [0] + [ord(x) & 31 for x in hrp]
 
 
-def bech32_verify_checksum(hrp, data):
+def bech32_verify_checksum(hrp: str, data: list[int]) -> Encoding:
     """Verify a checksum given HRP and converted data characters."""
-    return bech32_polymod(bech32_hrp_expand(hrp) + data) == 1
+    const = bech32_polymod(bech32_hrp_expand(hrp) + data)
+    if const == 1:
+        return Encoding.BECH32
+    if const == BECH32M_CONST:
+        return Encoding.BECH32M
+    raise ValueError("Invalid or unknown checksum")
 
 
-def bech32_create_checksum(hrp, data):
+def bech32_create_checksum(hrp: str, data: list[int], spec: Encoding) -> list[int]:
     """Compute the checksum values given HRP and data."""
     values = bech32_hrp_expand(hrp) + data
-    polymod = bech32_polymod(values + [0, 0, 0, 0, 0, 0]) ^ 1
+    const = BECH32M_CONST if spec == Encoding.BECH32M else 1
+    polymod = bech32_polymod(values + [0, 0, 0, 0, 0, 0]) ^ const
     return [(polymod >> 5 * (5 - i)) & 31 for i in range(6)]
 
 
-def bech32_encode(hrp, data):
+def bech32_encode(hrp: str, data: list[int], spec: Encoding) -> str:
     """Compute a Bech32 string given HRP and data values."""
-    combined = data + bech32_create_checksum(hrp, data)
+    combined = data + bech32_create_checksum(hrp, data, spec)
     return hrp + "1" + "".join([CHARSET[d] for d in combined])
 
 
-def bech32_decode(bech):
-    """Validate a Bech32 string, and determine HRP and data."""
+def bech32_decode(bech: str, max_bech_len: int = 90) -> tuple[str, list[int], Encoding]:
+    """Validate a Bech32/Bech32m string, and determine HRP and data."""
     if (any(ord(x) < 33 or ord(x) > 126 for x in bech)) or (
         bech.lower() != bech and bech.upper() != bech
     ):
-        return (None, None)
+        raise ValueError("invalid character in Bech32 string")
     bech = bech.lower()
     pos = bech.rfind("1")
-    if pos < 1 or pos + 7 > len(bech) or len(bech) > 90:
-        return (None, None)
+    if pos < 1 or pos + 7 > len(bech) or len(bech) > max_bech_len:
+        raise ValueError("invalid Bech32 string length")
     if not all(x in CHARSET for x in bech[pos + 1 :]):
-        return (None, None)
+        raise ValueError("invalid character in Bech32 data part")
     hrp = bech[:pos]
     data = [CHARSET.find(x) for x in bech[pos + 1 :]]
-    if not bech32_verify_checksum(hrp, data):
-        return (None, None)
-    return (hrp, data[:-6])
+    spec = bech32_verify_checksum(hrp, data)
+    return hrp, data[:-6], spec
 
 
-def convertbits(data, frombits, tobits, pad=True):
-    """General power-of-2 base conversion."""
+def convertbits(
+    data: t.Sequence[int], frombits: int, tobits: int, arbitrary_input: bool = True
+) -> list[int]:
+    """General power-of-2 base conversion.
+
+    The `arbitrary_input` parameter specifies what happens when the total length
+    of input bits is not a multiple of `tobits`.
+    If True (default), the overflowing bits are zero-padded to the right.
+    If False, the input must must be a valid output of `convertbits()` in the opposite
+    direction.
+    Namely:
+    (a) the overflow must only be the zero padding
+    (b) length of the overflow is less than `frombits`, meaning that there is no
+        additional all-zero `frombits`-sized group at the end.
+    If both conditions hold, the all-zero overflow is discarded.
+    Otherwise a ValueError is raised.
+    """
     acc = 0
     bits = 0
     ret = []
@@ -87,38 +122,58 @@ def convertbits(data, frombits, tobits, pad=True):
     max_acc = (1 << (frombits + tobits - 1)) - 1
     for value in data:
         if value < 0 or (value >> frombits):
-            return None
+            raise ValueError("input value does not match frombits size")
         acc = ((acc << frombits) | value) & max_acc
         bits += frombits
         while bits >= tobits:
             bits -= tobits
             ret.append((acc >> bits) & maxv)
-    if pad:
+
+    if arbitrary_input:
         if bits:
+            # append remaining bits, zero-padded from right
             ret.append((acc << (tobits - bits)) & maxv)
     elif bits >= frombits or ((acc << (tobits - bits)) & maxv):
-        return None
+        # (1) either there is a superfluous group at end of input, and/or
+        # (2) the remainder is nonzero
+        raise ValueError("padding mismatch")
+
     return ret
 
 
-def decode(hrp, addr):
+def decode(hrp: str, addr: str) -> tuple[int, bytes]:
     """Decode a segwit address."""
-    hrpgot, data = bech32_decode(addr)
+    hrpgot, data, spec = bech32_decode(addr)
+    # the following two lines are strictly not required
+    # but they make typecheckers happy
+    if data is None:
+        raise ValueError("invalid Bech32 encoding")
     if hrpgot != hrp:
-        return (None, None)
-    decoded = convertbits(data[1:], 5, 8, False)
-    if decoded is None or len(decoded) < 2 or len(decoded) > 40:
-        return (None, None)
+        raise ValueError("mismatched HRP")
+    decoded = bytes(convertbits(data[1:], 5, 8, False))
+    if not 2 <= len(decoded) <= 40:
+        raise ValueError("invalid segwit address data length")
     if data[0] > 16:
-        return (None, None)
-    if data[0] == 0 and len(decoded) != 20 and len(decoded) != 32:
-        return (None, None)
-    return (data[0], bytes(decoded))
+        raise ValueError("invalid segwit address")
+    if data[0] == 0 and len(decoded) not in (20, 32):
+        raise ValueError("invalid segwit v0 address length")
+    if (
+        data[0] == 0
+        and spec != Encoding.BECH32
+        or data[0] != 0
+        and spec != Encoding.BECH32M
+    ):
+        raise ValueError("invalid segwit address encoding")
+    return data[0], decoded
 
 
-def encode(hrp, witver, witprog):
+def encode(hrp: str, witver: int, witprog: bytes) -> str:
     """Encode a segwit address."""
-    ret = bech32_encode(hrp, [witver] + convertbits(witprog, 8, 5))
-    if decode(hrp, ret) == (None, None):
-        return None
-    return ret
+    data = convertbits(witprog, 8, 5)
+    spec = Encoding.BECH32 if witver == 0 else Encoding.BECH32M
+    ret = bech32_encode(hrp, [witver] + data, spec)
+    try:
+        decode(hrp, ret)  # check validity
+        return ret
+    except ValueError as e:
+        raise ValueError(f"failed to encode segwit address") from e

51873d98b7a3b096af4494ddcb2da1fe2cf2702c
matejcik <ja@matejcik.cz>
2022-05-05 11:59:44 +0200

    feat: use ecdsa+gmpy2 instead of fastecdsa (it's faster!)

--- src/bitcoinformats/bip32.py
+++ src/bitcoinformats/bip32.py
@@ -2,25 +2,39 @@ from __future__ import annotations
 
 import hmac
 import hashlib
-from dataclasses import dataclass, replace, asdict
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
 from typing_extensions import Self
 
 import construct as c
-from fastecdsa.curve import secp256k1
-from fastecdsa.keys import get_public_key
-from fastecdsa.point import Point
-from fastecdsa.encoding.sec1 import SEC1Encoder
+from ecdsa.curves import SECP256k1
+from ecdsa.ellipticcurve import INFINITY, Point
 
 from .struct import Struct
 from .base58 import b58check_decode, b58check_encode
 from .utils import hash160
 
+if TYPE_CHECKING:
+    from gmpy2 import mpz
+
 HARDENED_FLAG = 0x8000_0000
 
 
-def calculate_public_key(private_key: int) -> bytes:
-    pubkey = get_public_key(private_key, secp256k1)
-    return SEC1Encoder.encode_public_key(pubkey)
+def _pub_encode(public_key: Point) -> bytes:
+    return public_key.to_bytes("compressed")
+
+def _pub_decode(data: bytes) -> Point:
+    return Point.from_bytes(SECP256k1, "compressed")
+
+def _prv_encode(private_key: int | mpz) -> bytes:
+    return int(private_key).to_bytes(32, "big")
+
+def _prv_decode(data: bytes) -> int:
+    return int.from_bytes(data, "big")
+
+def _calculate_pubkey(privkey: int | mpz) -> Point:
+    """Calculate the public key from a private key."""
+    return SECP256k1.generator * privkey
 
 
 @dataclass
@@ -60,7 +74,7 @@ class ExtendedKey:
     fingerprint: bytes
     child_num: int
     chain_code: bytes
-    public_key: bytes
+    public_key: Point
 
     def to_xpub(self, version: int = 0x0488_B21E) -> Xpub:
         return Xpub(
@@ -69,7 +83,7 @@ class ExtendedKey:
             fingerprint=self.fingerprint,
             child_num=self.child_num,
             chain_code=self.chain_code,
-            key_bytes=self.public_key,
+            key_bytes=_pub_encode(self.public_key),
         )
 
     @staticmethod
@@ -81,7 +95,7 @@ class ExtendedKey:
             child_num=node.child_num,
             chain_code=node.chain_code,
             fingerprint=node.fingerprint,
-            public_key=node.key_bytes,
+            public_key=_pub_decode(node.key_bytes),
         )
 
     def public_child(self, i: int) -> ExtendedKey:
@@ -90,34 +104,32 @@ class ExtendedKey:
 
         i_as_bytes = i.to_bytes(4, "big")
 
+        pubkey_bytes = _pub_encode(self.public_key)
+
         # Public derivation
-        data = self.public_key + i_as_bytes
+        data = pubkey_bytes + i_as_bytes
 
         I64 = hmac.HMAC(
             key=self.chain_code, msg=data, digestmod=hashlib.sha512
         ).digest()
         I_left_as_exponent = int.from_bytes(I64[:32], "big")
-        if I_left_as_exponent >= secp256k1.q:
-            raise ValueError("Derivation results in invalid key (I_left >= q)")
+        if I_left_as_exponent >= SECP256k1.order:
+            raise ValueError("Derivation results in invalid key (I_left >= order)")
 
-        k_par = SEC1Encoder.decode_public_key(self.public_key, secp256k1)
         # point(parse256(I_left)) + Kpar
-        result = I_left_as_exponent * secp256k1.G + k_par
+        result = I_left_as_exponent * SECP256k1.generator + self.public_key
 
-        if k_par == Point.IDENTITY_ELEMENT:
+        if result == INFINITY:
             raise ValueError(
                 "Derivation results in invalid key (public key is infinity)"
             )
-
-        # Convert public point to compressed public key
-        public_key = SEC1Encoder.encode_public_key(result)
 
         return ExtendedKey(
             depth=self.depth + 1,
             child_num=i,
             chain_code=I64[32:],
-            fingerprint=hash160(self.public_key)[:4],
-            public_key=public_key,
+            fingerprint=hash160(pubkey_bytes)[:4],
+            public_key=result,
         )
 
     def derive(self, path: list[int]) -> Self:
@@ -129,11 +141,11 @@ class ExtendedKey:
 
 @dataclass(frozen=True)
 class ExtendedPrivateKey(ExtendedKey):
-    private_key: int
+    private_key: int | mpz
 
     def _serialize_private_key(self) -> bytes:
         assert self.private_key is not None
-        return b"\x00" + self.private_key.to_bytes(32, "big")
+        return b"\x00" + _prv_encode(self.private_key)
 
     def to_xprv(self, version: int = 0x0488_ADE4) -> Xpub:
         return Xpub(
@@ -149,31 +161,32 @@ class ExtendedPrivateKey(ExtendedKey):
     def from_xprv(node: Xpub) -> ExtendedPrivateKey:
         if node.key_bytes[0] != 0:
             raise ValueError("Xpub provided, use ExtendedKey.from_xpub()")
-        private_key = int.from_bytes(node.key_bytes[1:], "big")
+        private_key = _prv_decode(node.key_bytes[1:])
         return ExtendedPrivateKey(
             depth=node.depth,
             child_num=node.child_num,
             chain_code=node.chain_code,
             fingerprint=node.fingerprint,
             private_key=private_key,
-            public_key=calculate_public_key(private_key),
+            public_key=_calculate_pubkey(private_key),
         )
 
     def private_child(self, i: int) -> ExtendedPrivateKey:
         i_as_bytes = i.to_bytes(4, "big")
+        pubkey_bytes = _pub_encode(self.public_key)
         if i & HARDENED_FLAG:
             data = self._serialize_private_key() + i_as_bytes
         else:
-            data = self.public_key + i_as_bytes
+            data = pubkey_bytes + i_as_bytes
         I64 = hmac.HMAC(
             key=self.chain_code, msg=data, digestmod=hashlib.sha512
         ).digest()
 
         I_left = int.from_bytes(I64[:32], "big")
-        if I_left >= secp256k1.q:
-            raise ValueError("Derivation results in invalid key (I_left >= q)")
+        if I_left >= SECP256k1.order:
+            raise ValueError("Derivation results in invalid key (I_left >= order)")
 
-        private_key = (self.private_key + I_left) % secp256k1.q
+        private_key = (self.private_key + I_left) % SECP256k1.order
         if private_key == 0:
             raise ValueError("Derivation results in invalid key (private key is zero)")
 
@@ -181,18 +194,18 @@ class ExtendedPrivateKey(ExtendedKey):
             depth=self.depth + 1,
             child_num=i,
             chain_code=I64[32:],
-            fingerprint=hash160(self.public_key)[:4],
+            fingerprint=hash160(pubkey_bytes)[:4],
             private_key=private_key,
-            public_key=calculate_public_key(private_key),
+            public_key=_calculate_pubkey(private_key),
         )
 
     @classmethod
     def master_key(cls, seed: bytes, seed_salt: bytes = b"Bitcoin seed") -> Self:
         I64 = hmac.HMAC(key=seed_salt, msg=seed, digestmod=hashlib.sha512).digest()
         I_left = int.from_bytes(I64[:32], "big")
-        if not 0 < I_left < secp256k1.q:
+        if not 0 < I_left < SECP256k1.order:
             raise ValueError(
-                "Derivation results in invalid key (either zero or greater than q)"
+                "Derivation results in invalid key (either zero or greater than order)"
             )
         return cls(
             depth=0,
@@ -200,7 +213,7 @@ class ExtendedPrivateKey(ExtendedKey):
             chain_code=I64[32:],
             fingerprint=b"\x00\x00\x00\x00",
             private_key=I_left,
-            public_key=calculate_public_key(I_left),
+            public_key=_calculate_pubkey(I_left),
         )
 
     def derive(self, path: list[int]) -> Self:
